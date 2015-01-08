f650efba0ef618fe67a107caee6dd9e2961233ca
Mark Platt <mark@mrkplt.com>
2015-01-07 22:18:52 -0500

    fix weird formatting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,8 +6,10 @@ Time.zone = 'UTC'
 
 ActiveRecord::Base.default_timezone = 'UTC'
 
-ActiveRecord::Base.establish_connection(:adapter => "sqlite3",
-                                        :database => File.dirname(__FILE__) + "/time_jawn.sqlite3")
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => File.dirname(__FILE__) + "/time_jawn.sqlite3"
+)
 
 load File.dirname(__FILE__) + '/support/schema.rb'
 load File.dirname(__FILE__) + '/support/models.rb'
